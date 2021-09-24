573aa11c65508ff0fe097788a847fa18a1ce2bab
hifilorau <tomrau@gmail.com>
2021-09-24 16:10:42 -0400

    checkin stuff

---
+++ src/components/header.js
@@ -0,0 +1,43 @@
+
+import PropTypes from "prop-types"
+import React from "react"
+import {Link} from 'react-router-dom'
+
+const Header = ({ siteTitle }) => (
+  <header
+    style={{
+      background: `rebeccapurple`,
+      marginBottom: `1.45rem`,
+    }}
+  >
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `1.45rem 1.0875rem`,
+      }}
+    >
+      <h4 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            color: `white`,
+            textDecoration: `none`,
+          }}
+        >
+          For booking or to rent space contact ps37@gmail.com or drop us a dm on the insta @ps37durham
+        </Link>
+      </h4>
+    </div>
+  </header>
+)
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
---
+++ src/containers/Home/home.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Video from '../../images/ps37-v2-comp-nl.mp4';
+import Poster from '../../images/ps37-moon-shot.png'
+import Logo from "../../images/ps37-logo-trees.png"
+
+
+const Home = () => {
+
+
+  const rnGenerator = () => {
+    const rarity = 10;
+    let rng = Math.floor((Math.random() * rarity) + 1);
+    this.setState({randomNumber: rng})
+  }
+
+
+  return (
+  <div className="homepage-container">
+    <div className="video-wrapper">
+      <video className="video" src={Video} autoPlay loop muted poster={Poster}/>
+    </div>
+    <div className="landing-content-container">
+
+      <div className="logo-wrapper">
+        <img className="logo-landing glitch" src={Logo}/>
+        {/* { this.state.randomNumber == 3 ?  <div className="tagline">A Paradise in Space</div>  : null }  */}
+        {/* { this.state.randomNumber !== 3 ? <div className="tagline">makerspace, office, and arthaüs</div> : null } */}
+        <div className="tagline">makerspace, event venue, and arthaüs</div> 
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default Home 
